ab5947206808565299eed449842d7f309517d450
Imoh Henry <henryimoh2@gmail.com>
2020-09-29 14:44:15 +0100

    Add files via upload

---
+++ contact_process.php
@@ -0,0 +1,63 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/PHPMailer-master/src/Exception.php';
+require_once __DIR__ . '/PHPMailer-master/src/PHPMailer.php';
+require_once __DIR__ . '/PHPMailer-master/src/SMTP.php';
+
+
+if(isset($_POST['submit'])){
+	$to = "henry_imoh@yahoo.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+	$subject = $_REQUEST['subject'];
+	$location = $_REQUEST['location']
+    $number = $_REQUEST['number'];
+    $cmessage = $_REQUEST['message'];
+
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->Username = 'Henryimoh2@gmail.com'; // YOUR gmail email
+	$mail->Password = 'sharon9358'; // YOUR gmail password
+
+	$mail->setFrom($email, 'Shecooks');
+    $mail->addAddress('Henryimoh2@gmail.com', 'Me');
+    $mail->Subject = 'New message from Get2work';
+
+	require 'PHPMailerAutoload.php';
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $subject = "You have a message from A user.";
+
+    $logo = 'img/logo.png';
+    $link = 'Smtp.gmail.com';
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+}
+?>
